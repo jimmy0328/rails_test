31077ef89f516dc38621243c574c5bf6297bfd3e
jimmy <jimmy0328@gmail.com>
2016-11-23 11:08:48 +0800

    add minitest product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 1}
+  validates :title, uniqueness: true
+
+
 end
